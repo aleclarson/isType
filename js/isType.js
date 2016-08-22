0eb3e90fd967d036b2d9b329fae4ad00faf12fb4
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-22 11:45:16 -0700

    Remove support for arrays of types
    TIP: Use the 'Typle' validator instead!
    You can also use 'Array.or(Object, Number)' to create typles.

--- js/isType.js
+++ js/isType.js
@@ -1,13 +1,10 @@
-var Validator, checkTypes, isType;
+var Validator, isType;
 
 Validator = require("Validator");
 
 isType = function(value, type) {
   if (type instanceof Validator) {
     return type.test(value);
-  }
-  if (Array.isArray(type)) {
-    return checkTypes(value, type);
   }
   if (value == null) {
     return false;
@@ -16,25 +13,5 @@ isType = function(value, type) {
 };
 
 module.exports = isType;
-
-checkTypes = function(value, types) {
-  var index, numTypes, type;
-  numTypes = types.length;
-  index = -1;
-  while (++index < numTypes) {
-    type = types[index];
-    if (type instanceof Validator) {
-      if (type.test(value)) {
-        return true;
-      }
-    }
-    if (value != null) {
-      if (type === value.constructor) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
 
 //# sourceMappingURL=map/isType.map
--- src/isType.coffee
+++ src/isType.coffee
@@ -6,31 +6,9 @@ isType = (value, type) ->
   if type instanceof Validator
     return type.test value
 
-  if Array.isArray type
-    return checkTypes value, type
-
   if not value?
     return no
 
   return type is value.constructor
 
 module.exports = isType
-
-#
-# Helpers
-#
-
-checkTypes = (value, types) ->
-  numTypes = types.length
-
-  index = -1
-  while ++index < numTypes
-    type = types[index]
-
-    if type instanceof Validator
-      return yes if type.test value
-
-    if value?
-      return yes if type is value.constructor
-
-  return no
